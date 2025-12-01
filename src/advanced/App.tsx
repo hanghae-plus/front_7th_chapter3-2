08b84c1ff97e705b5e1a9ab99d92f365aab55a4d
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2025-12-01 23:05:54 +0900

    배포테스트

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,10 +1,11 @@
-import { Provider } from 'jotai';
-import { PagesLayout } from './components/layout/PagesLayout';
+import { Provider } from "jotai";
+import { PagesLayout } from "./components/layout/PagesLayout";
 
 // 메인 App 컴포넌트
 const App = () => {
   return (
     <Provider>
+      <div>advanced</div>
       <PagesLayout />
     </Provider>
   );
--- src/basic/App.tsx
+++ src/basic/App.tsx
@@ -1,8 +1,12 @@
-import { PagesLayout } from './components/layout/PagesLayout';
+import { PagesLayout } from "./components/layout/PagesLayout";
 
 // 메인 App 컴포넌트
 const App = () => {
-  return <PagesLayout />;
+  return;
+  <>
+    <div>베이직</div>
+    <PagesLayout />;
+  </>;
 };
 
 export default App;
