b51a00954b49462566155d3459005527c3a53053
junijaei <hjhj7895598@gmail.com>
2025-12-01 21:55:43 +0900

    feat: utils 함수 개발
    - validator
    - formatters
    - useDebounce
    - useLocalStorage

---
+++ src/basic/utils/validators.ts
@@ -0,0 +1,20 @@
+// TODO: 검증 유틸리티 함수들
+// 구현할 함수:
+// - isValidCouponCode(code: string): boolean - 쿠폰 코드 형식 검증 (4-12자 영문 대문자와 숫자)
+// - isValidStock(stock: number): boolean - 재고 수량 검증 (0 이상)
+// - isValidPrice(price: number): boolean - 가격 검증 (양수)
+// - extractNumbers(value: string): string - 문자열에서 숫자만 추출
+
+// TODO: 구현
+export const isValidCouponCode = (code: string) => {
+  return /^[A-Z0-9]{4,12}$/.test(code);
+};
+export const isValidStock = (stock: number) => {
+  return stock > 0;
+};
+export const isValidPrice = (price: number) => {
+  return price >= 0;
+};
+export const extractNumbers = (value: string) => {
+  return Number(value.replace(/[^0-9]/g, ''));
+};
