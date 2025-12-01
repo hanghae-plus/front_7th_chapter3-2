5e8ab27485d6085ef8f677d13147837d2747a46e
hyeongwoo <guddn0625@gmail.com>
2025-12-01 10:39:23 +0900

    pnpm dev 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    server: {
+      host: 'localhost',
+      port: 5173,
+      open: true,
+    },
   }),
   defineTestConfig({
     test: {
