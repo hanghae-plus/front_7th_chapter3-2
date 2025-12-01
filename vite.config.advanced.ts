1ae2ed96986cb50c1c6ab448bc10bc2a0ba1095d
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2025-12-01 23:26:04 +0900

    배포테스트

--- vite.config.advanced.ts
+++ vite.config.advanced.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/advanced/",
+  base: "/front_7th_chapter3-2/advanced/",
   build: {
     outDir: "dist/advanced",
     emptyOutDir: false,
--- vite.config.basic.ts
+++ vite.config.basic.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/front_7th_chapter3-2/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
