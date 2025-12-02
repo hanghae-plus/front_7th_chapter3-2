5c196da7e7a89cb2f5cdb550bdead7d49d70ba67
ahnsummer <bazaar9095@gmail.com>
2025-12-02 23:10:31 +0900

    refactor. basic: UI 중심으로 shop 페이지 컴포넌트 분리

---
+++ src/basic/domains/products/utils/getRemainingStock.ts
@@ -0,0 +1,8 @@
+import { CartItem, Product } from "../../../../types";
+
+export const getRemainingStock = (product: Product, cart: CartItem[]): number => {
+  const cartItem = cart.find((item) => item.product.id === product.id);
+  const remaining = product.stock - (cartItem?.quantity || 0);
+
+  return remaining;
+};
