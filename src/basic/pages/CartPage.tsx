62ff43501e230476aea1f02bb6cbacb29a6cb925
Woojung Kim <60292183+kimfriendship@users.noreply.github.com>
2025-12-03 19:32:50 +0900

    refactor: 불필요한 컴포넌트 분리 삭제

--- src/basic/pages/CartPage.tsx
+++ src/basic/pages/CartPage.tsx
@@ -1,6 +1,9 @@
-import { ProductList } from "../widgets/product/ui/ProductList";
 import { CartItem, Coupon, Product, ProductWithUI } from "../types";
-import { PaymentBanner } from "../widgets/payment/ui/PaymentBanner";
+import { CartList } from "../widgets/cart/ui/CartList";
+import { CouponSelector } from "../widgets/coupon/ui/CouponSelector";
+import { CartPayment } from "../widgets/cart/ui/CartPayment";
+import { calculateCartTotal } from "../features/calculate-price/model/calculateCartTotal";
+import { ProductCard } from "../entities/product/ui/ProductCard";
 
 export function CartPage({
   products,
@@ -36,29 +39,61 @@ export function CartPage({
   return (
     <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
       <div className="lg:col-span-3">
-        {/* 상품 목록 */}
-        <ProductList
-          products={products}
-          filteredProducts={filteredProducts}
-          debouncedSearchTerm={debouncedSearchTerm}
-          formatPrice={formatPrice}
-          getRemainingStock={getRemainingStock}
-          addToCart={addToCart}
-        />
+        <section>
+          <div className="mb-6 flex justify-between items-center">
+            <h2 className="text-2xl font-semibold text-gray-800">전체 상품</h2>
+            <div className="text-sm text-gray-600">
+              총 {products.length}개 상품
+            </div>
+          </div>
+          {filteredProducts.length === 0 ? (
+            <div className="text-center py-12">
+              <p className="text-gray-500">
+                "{debouncedSearchTerm}"에 대한 검색 결과가 없습니다.
+              </p>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {filteredProducts.map((product) => {
+                return (
+                  <ProductCard
+                    key={product.id}
+                    product={product}
+                    formatPrice={formatPrice}
+                    getRemainingStock={getRemainingStock}
+                    addToCart={addToCart}
+                  />
+                );
+              })}
+            </div>
+          )}
+        </section>
       </div>
 
       <div className="lg:col-span-1">
         <div className="sticky top-24 space-y-4">
-          <PaymentBanner
-            cart={cart}
-            removeFromCart={removeFromCart}
-            updateQuantity={updateQuantity}
-            coupons={coupons}
-            selectedCoupon={selectedCoupon}
-            setSelectedCoupon={setSelectedCoupon}
-            applyCoupon={applyCoupon}
-            completeOrder={completeOrder}
-          />
+          <>
+            <CartList
+              cart={cart}
+              removeFromCart={removeFromCart}
+              updateQuantity={updateQuantity}
+            />
+
+            {cart.length > 0 && (
+              <>
+                <CouponSelector
+                  coupons={coupons}
+                  selectedCoupon={selectedCoupon}
+                  setSelectedCoupon={setSelectedCoupon}
+                  applyCoupon={applyCoupon}
+                />
+                <CartPayment
+                  totals={calculateCartTotal(cart, selectedCoupon)}
+                  completeOrder={completeOrder}
+                />
+              </>
+            )}
+          </>
         </div>
       </div>
     </div>
