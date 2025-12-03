b60c638b2e6dc4a756bbef03e65a9cf0c7322c53
ahnsummer <bazaar9095@gmail.com>
2025-12-03 14:18:36 +0900

    feat. useLocalStorage 훅 개발

---
+++ src/basic/shared/hooks/useLocalStorage.ts
@@ -0,0 +1,40 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() => {
+    const saved = localStorage.getItem(key);
+    if (saved) {
+      return JSON.parse(saved);
+    }
+    return initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    window.addEventListener(
+      "storage",
+      (event) => {
+        if (event.key === key && event.newValue != null) {
+          setValue(JSON.parse(event.newValue));
+        }
+      },
+      {
+        signal: controller.signal,
+      }
+    );
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return [value, setValue];
+}
