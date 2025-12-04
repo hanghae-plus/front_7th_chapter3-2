ae5ebdff6f992c79a63cbbed7f542ecfc0c4627a
jy0813 <busbug3@gmail.com>
2025-12-04 16:52:17 +0900

    feat: coupon 순수함수 추가

---
+++ src/basic/entities/coupon/lib/validation.ts
@@ -0,0 +1,15 @@
+import { COUPON_CONFIG } from '../config';
+import { Coupon } from '../model/types';
+
+export const canApplyCoupon = (
+  coupon: Coupon,
+  totalAmount: number,
+): boolean => {
+  if (
+    coupon.discountType === 'percentage' &&
+    totalAmount < COUPON_CONFIG.MIN_ORDER_FOR_PERCENTAGE_DISCOUNT
+  ) {
+    return false;
+  }
+  return true;
+};
