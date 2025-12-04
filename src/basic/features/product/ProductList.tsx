9ad89eab970044f26919a39b7531af07c3e7cb1b
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-12-05 01:40:49 +0900

    refactor: ProductList 내  컴포넌트 분리

--- src/basic/features/product/ProductList.tsx
+++ src/basic/features/product/ProductList.tsx
@@ -1,6 +1,35 @@
 import { Product } from '../../../types';
 import { ProductItem } from './ProductItem';
 
+const ProductListHeader = ({
+  productsTotalCount,
+}: {
+  productsTotalCount: number;
+}) => {
+  return (
+    <div className="mb-6 flex justify-between items-center">
+      <h2 className="text-2xl font-semibold text-gray-800">전체 상품</h2>
+      <div className="text-sm text-gray-600">
+        총 {productsTotalCount}개 상품
+      </div>
+    </div>
+  );
+};
+
+const ProductListEmpty = ({
+  debouncedSearchTerm,
+}: {
+  debouncedSearchTerm: string;
+}) => {
+  return (
+    <div className="text-center py-12">
+      <p className="text-gray-500">
+        "{debouncedSearchTerm}"에 대한 검색 결과가 없습니다.
+      </p>
+    </div>
+  );
+};
+
 export const ProductList = ({
   products,
   debouncedSearchTerm,
@@ -38,25 +67,14 @@ export const ProductList = ({
 
   return (
     <div className="lg:col-span-3">
-      {/* 상품 목록 */}
+      <ProductListHeader productsTotalCount={products.length} />
       <section>
-        <div className="mb-6 flex justify-between items-center">
-          <h2 className="text-2xl font-semibold text-gray-800">전체 상품</h2>
-          <div className="text-sm text-gray-600">
-            총 {products.length}개 상품
-          </div>
-        </div>
         {filteredProducts.length === 0 ? (
-          <div className="text-center py-12">
-            <p className="text-gray-500">
-              "{debouncedSearchTerm}"에 대한 검색 결과가 없습니다.
-            </p>
-          </div>
+          <ProductListEmpty debouncedSearchTerm={debouncedSearchTerm} />
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {filteredProducts.map((product) => {
               const remainingStock = getRemainingStock(product);
-
               return (
                 <ProductItem
                   product={product}
