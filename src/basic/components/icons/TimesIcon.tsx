4e704aed182899075fedcdfb23119e83915aa540
Leehyunji <hjlee@bizflow.com>
2025-12-04 21:42:42 +0900

    feat: Icons

--- src/basic/components/icons/TimesIcon.tsx
+++ src/basic/components/icons/TimesIcon.tsx
@@ -1,6 +1,6 @@
-export const TimesIcon = () => {
+export const TimesIcon = ({ size }: { size: number }) => {
   return (
-    <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <svg className={`w-${size} h-${size}`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
     </svg>
   );
--- src/basic/components/icons/index.ts
+++ src/basic/components/icons/index.ts
@@ -2,3 +2,5 @@ export { TimesIcon } from "./TimesIcon";
 export { CartIcon } from "./CartIcon";
 export { TrashIcon } from "./TrashIcon";
 export { PlusIcon } from "./PlusIcon";
+export { BagIcon } from "./BagIcon";
+export { ImageIcon } from "./ImageIcon";
