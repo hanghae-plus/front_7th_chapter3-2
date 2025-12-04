34fe111ff137b35cb74dc0148e9dc8198f97b409
jinseoit <jinseoit@gmail.com>
2025-12-04 19:51:05 +0900

    refactor : 미사용 hook제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const base: string = process.env.NODE_ENV === "production" ? "/front_7th_chapter3-2/" : "";
 
 export default mergeConfig(
   defineConfig({
+    base,
     plugins: [react()],
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
