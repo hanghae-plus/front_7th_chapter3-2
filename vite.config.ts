14f38c6c9a66156af6e5f03a7d4238acb72e68e6
grappe96 <w00k0805@naver.com>
2025-12-05 04:21:09 +0900

    fix: 배포 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,17 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: './index.basic.html',
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
   })
-)
+);
