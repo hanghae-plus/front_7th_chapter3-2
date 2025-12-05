38747043685cb18ce3cc0cb48a67f12d468e5b8c
Yongtae Park <samuel.park@mz.co.kr>
2025-12-05 16:38:32 +0900

    build: set base URL for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const base: string = process.env.NODE_ENV === 'production' ? '/frontend_7th_chapter3-2/' : '';
+
 export default mergeConfig(
   defineConfig({
+    base,
     plugins: [react()],
     build: {
       outDir: 'dist',
