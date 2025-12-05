f684ea8fb476cf3c8a4a088f9e4675e9786af0fd
yrnoh <yrnoh@rsupport.com>
2025-12-05 09:22:57 +0900

    chore: Prettier, tsconfig, vite 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default mergeConfig(
   defineConfig({
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
