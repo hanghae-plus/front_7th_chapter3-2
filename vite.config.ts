cd74704ea86d3b941f1d9a09efc070b49adaecb4
김민석 <kms951018@douzone.com>
2025-12-06 01:54:21 +0900

    chore: 배포파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
+    base: "/front_7th_chapter3-2/",
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: "./index.advanced.html",
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
